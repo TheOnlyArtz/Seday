aa3c5ea841d43b94f7b9ebbd47fadffd53f334c1
callcraft456@gmail.com <callcraft456@gmail.com>
2019-04-16 19:44:26 +0300

    "Intensive" indentations

--- src/Website/Database/updator.js
+++ src/Website/Database/updator.js
@@ -3,9 +3,9 @@ const updators = require('./updators')
 async function updator (rethinkDB, eventName, value, guildID) {
 
     switch (eventName) {
-            case "new prefix":
-                await updators.updatePrefix(rethinkDB, value, guildID);
-                break;
+      case "new prefix":
+        await updators.updatePrefix(rethinkDB, value, guildID);
+        break;
     }
 }
 
