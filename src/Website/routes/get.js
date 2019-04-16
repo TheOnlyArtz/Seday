ce621610e66eda3ba3421c23f42ec05e979e0bba
callcraft456@gmail.com <callcraft456@gmail.com>
2019-04-16 10:39:34 +0300

    Cleaned up the code, separated functions

--- src/Website/routes/get.js
+++ src/Website/routes/get.js
@@ -12,7 +12,7 @@ const mainFunctions = require('../main.js');
 const r = mainFunctions['rethink'];
 
 Router.get('/', ifLoggedMiddleware, function(req, res) {
-  res.render('Home', {user: req.session.user, checkforperm: hasServerManagPerms})
+  res.render('Home', {user: req.session.user, checkforperm: hasServerManagePerms})
 })
 
 Router.get('/login', function(req, res) {
@@ -33,11 +33,11 @@ Router.get('/me', ifLoggedMiddleware, function(req, res) {
   return res.set(200).send({user: req.session.user, auth: req.session.auth});
 });
 
-
 Router.get('/dashboard/:guildID?', ifLoggedMiddleware, async function(req, res) {
-  let founded = req.session.user.guilds.find(i => i.id === req.params.guildID)
-  if (!req.session.user.guilds.filter(i => i.id === req.params.guildID)[0] || hasServerManagPerms(founded.permissions) === false) {
-      return res.set(401).send({message: 'Forbidden! You don\'t have permissions to edit this server\'s config', status: 401})
+  let user = req.session.user.guilds.find(i => i.id === req.params.guildID);
+  if (!user || !hasServerManagePerms(req, user.permissions, true)) {
+      return res.set(401)
+      .send({message: 'Forbidden! You don\'t have permissions to edit this server\'s config', status: 401});
   }
   const guildData = await r.table('Guilds').get(req.params.guildID)
   res.render('Dashboard', {
@@ -59,10 +59,16 @@ function ifLoggedMiddleware(req, res, next) {
   res.redirect('/login');
 }
 
-function hasServerManagPerms(memberPerms) {
+function hasServerManagePerms({session, params}, memberPerms, advancedLookup) {
   for (let perm of [32]) {
     if ((memberPerms & perm) !== perm) {
       return false
+    }
+  }
+  if (advancedLookup) {
+    let inGuild = session.user.guilds.filter(i => i.id === params.guildID)[0];
+    if (!inGuild) {
+      return false;
     }
   }
 
@@ -78,20 +84,20 @@ function initData(req, res) {
     code: req.query.code,
   }
 
-    post("https://discordapp.com/api/oauth2/token").send(requestPayload)
-      .headers({
-        "Content-Type": 'application/x-www-form-urlencoded',
-        "User-Agent": 'DiscordBot'
+  post("https://discordapp.com/api/oauth2/token").send(requestPayload)
+    .headers({
+      "Content-Type": 'application/x-www-form-urlencoded',
+      "User-Agent": 'DiscordBot'
+      })
+    .end(function (response) {
+      const {access_token, refresh_token, expires_in} = response.body;
+      get("https://discordapp.com/api/users/@me").headers({'Authorization': `${token_type} ${access_token}`}).end(function(user) {
+        get("https://discordapp.com/api/users/@me/guilds").headers({'Authorization': `${token_type} ${access_token}`}).end(function(guilds) {
+          req.session.auth = response['body'];
+          req.session.user = user['body'];
+          req.session.user.guilds = guilds['body'];
+          res.redirect('/');
         })
-      .end(function (response) {
-        const {access_token, refresh_token, expires_in} = response.body;
-        get("https://discordapp.com/api/users/@me").headers({'Authorization': `${token_type} ${access_token}`}).end(function(user) {
-          get("https://discordapp.com/api/users/@me/guilds").headers({'Authorization': `${token_type} ${access_token}`}).end(function(guilds) {
-            req.session.auth = response['body'];
-            req.session.user = user['body'];
-            req.session.user.guilds = guilds['body'];
-            res.redirect('/');
-          })
-        })
-      });
+      })
+    });
 }
